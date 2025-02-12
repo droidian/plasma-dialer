f9d5aa32281b88403d0077425f28b07479fdd9cc
arpio23 <alex@familyrutz.com>
2025-02-12 08:46:31 -0700

    kde-telephony-daemon/notification-manager: fix missed calls not showing in notification history.

--- kde-telephony-daemon/src/notification-manager.cpp
+++ kde-telephony-daemon/src/notification-manager.cpp
@@ -118,7 +118,8 @@ void NotificationManager::onCallStateChanged(const DialerTypes::CallData &callDa
         if (callData.state == DialerTypes::CallState::Terminated) {
             handleCallInteraction();
 
-            if (callData.stateReason == DialerTypes::CallStateReason::Unknown && !m_callStarted) {
+            if ((callData.stateReason == DialerTypes::CallStateReason::Unknown 
+                    || callData.stateReason == DialerTypes::CallStateReason::TerminatedReason) && !m_callStarted) {
                 auto missedCallNotification = new KNotification(QStringLiteral("callMissed"), KNotification::Persistent, this);
                 missedCallNotification->setComponentName(QStringLiteral("plasma-dialer"));
                 missedCallNotification->setTitle(i18n("Missed call"));
